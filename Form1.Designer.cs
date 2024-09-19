e89cbef65f2d4c690788684dacb0185c741e906a
Luis Antonio Montero <64512126+nino52@users.noreply.github.com>
2024-09-18 21:22:29 -0400

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace CalculandoSalario
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtHorasTrabajadas = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cmbTipoEmpleado = new System.Windows.Forms.ComboBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.lblSalario = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(155, 126);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(230, 32);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Horas trabajadas";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtHorasTrabajadas
+            // 
+            this.txtHorasTrabajadas.Location = new System.Drawing.Point(161, 176);
+            this.txtHorasTrabajadas.Name = "txtHorasTrabajadas";
+            this.txtHorasTrabajadas.Size = new System.Drawing.Size(200, 22);
+            this.txtHorasTrabajadas.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(135, 30);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(250, 32);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Tipo de empleado:";
+            // 
+            // cmbTipoEmpleado
+            // 
+            this.cmbTipoEmpleado.FormattingEnabled = true;
+            this.cmbTipoEmpleado.Items.AddRange(new object[] {
+            "Docente por hora",
+            "Docente de contrato fijo",
+            "Empleado administrativo"});
+            this.cmbTipoEmpleado.Location = new System.Drawing.Point(161, 77);
+            this.cmbTipoEmpleado.Name = "cmbTipoEmpleado";
+            this.cmbTipoEmpleado.Size = new System.Drawing.Size(200, 24);
+            this.cmbTipoEmpleado.TabIndex = 3;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(351, 363);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(137, 59);
+            this.btnCalcular.TabIndex = 4;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // lblSalario
+            // 
+            this.lblSalario.AutoSize = true;
+            this.lblSalario.Font = new System.Drawing.Font("Clarendon Blk BT", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSalario.Location = new System.Drawing.Point(31, 266);
+            this.lblSalario.Name = "lblSalario";
+            this.lblSalario.Size = new System.Drawing.Size(132, 34);
+            this.lblSalario.TabIndex = 5;
+            this.lblSalario.Text = "Salario:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 406);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(101, 16);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "by Luis Montero";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(512, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblSalario);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.cmbTipoEmpleado);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtHorasTrabajadas);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Calcular Horas Trabajadas";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtHorasTrabajadas;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cmbTipoEmpleado;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Label lblSalario;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculandoSalario
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            double salario = 0;
+            double tarifaPorHora = 800;
+            double horasTrabajadas;
+            double bonificacion = 0.5; // 50% de bonificación
+            double meta = 1000; // Asumimos una meta para el cálculo
+
+            // Obtener horas trabajadas
+            if (double.TryParse(txtHorasTrabajadas.Text, out horasTrabajadas))
+            {
+                // Determinar el tipo de empleado
+                string tipoEmpleado = cmbTipoEmpleado.SelectedItem.ToString();
+
+                switch (tipoEmpleado)
+                {
+                    case "Docente por hora":
+                        salario = horasTrabajadas * tarifaPorHora;
+                        break;
+
+                    case "Docente de contrato fijo":
+                        salario = (horasTrabajadas >= meta) ? 2000 : 1000; // Asumimos un salario fijo
+                        break;
+
+                    case "Empleado administrativo":
+                        salario = (horasTrabajadas >= meta) ? 1500 : 750; // Asumimos un salario fijo
+                        break;
+                }
+
+                lblSalario.Text = $"Salario: {salario:C}"; // Muestra el salario en formato de moneda
+            }
+            else
+            {
+                MessageBox.Show("Por favor, ingresa un número válido de horas trabajadas.");
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
